7bb4500e8a677ea0f222a692803390ca2c61bbbc
yashchaure212 <yashchaure212@gmail.com>
2026-06-29 02:46:48 +0530

    final

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -68,9 +68,8 @@ export const register = async (req, res) => {
         // set cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
+            secure: true,
+            sameSite: "None",
         });
 
         // success response
@@ -134,9 +133,8 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
+            secure: true,
+            sameSite: "None",
         }).status(200).json({
             message: "Login successful",
             success: true,
