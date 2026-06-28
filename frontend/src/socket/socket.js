7fd0dd605f99c8947b08dd5e972e3827cf142d5a
yashchaure212 <yashchaure212@gmail.com>
2026-06-29 01:43:47 +0530

    Fix Socket.io production URL

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export const connectSocket = (userId) => {
   if (!socket) {
-    socket = io("http://localhost:5000", {
+    socket = io(import.meta.env.VITE_API_URL.replace("/api", ""), {
       query: {
         userId,
       },
